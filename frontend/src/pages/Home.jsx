6adb2ce54c76f264f0695b71a869f9c19fe2778f
CodeManBist <sagarbist372@gmail.com>
2026-04-07 13:35:33 +0530

    improved components file strcuture

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
-import Hero from '../components/Hero'
-import TechStack from '../components/TechStack'
-import Features from '../components/Features'
-import FeaturedWork from '../components/FeaturedWork'
-import Services from '../components/Services'
-import CTA from '../components/CTA'
-import Footer from '../components/Footer'
+import Navbar from '../components/common/Navbar'
+import Hero from '../components/home/Hero'
+import TechStack from '../components/home/TechStack'
+import Features from '../components/home/Features'
+import FeaturedWork from '../components/home/FeaturedWork'
+import Services from '../components/home/Services'
+import CTA from '../components/home/CTA'
+import Footer from '../components/common/Footer'
 
 const Home = () => {
   return (
